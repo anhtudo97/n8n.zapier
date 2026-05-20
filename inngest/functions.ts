5b6e687b0f452917f7e7c8ebe73e3ae7868f6087
Alex Dou <anhtudo97@gmail.com>
2026-05-21 01:56:26 +0700

    chore: update for production

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -17,7 +17,7 @@ import { slackChannel } from "./channels/slack"
 export const executeWorkflow = inngest.createFunction(
   {
     id: "execute-workflow",
-    retries: 0,
+    retries: process.env.NODE_ENV === "production" ? 3 : 0, // Retry up to 3 times in production, no retries in development
     onFailure: async ({ event }) => {
       const inngestEventId = event.data.event.id
       await prisma.execution.update({
